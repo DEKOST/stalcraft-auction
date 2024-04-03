2445a88576786641b7269f72caaacae679a1dd7b
DEKOST <121599819+DEKOST@users.noreply.github.com>
2024-04-03 10:12:10 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import convention
 import streamlit as st
 
-st.text('Fixed width text')
-st.title('My title')
-st.header('My header')
+st.title('Stalcraft Auction')
+st.header('Полная база данных!')
